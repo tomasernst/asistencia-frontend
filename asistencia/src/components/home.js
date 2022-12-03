13aa2e361f9fee55c46e2e68370cc9d19945a15a
Tomás Ernst <tomasernst@Tomass-MacBook-Pro.local>
2022-12-03 17:07:03 -0300

    feat: Routes and Home page

---
+++ asistencia/src/components/home.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <h1>Hola tío joder!</h1>
+        );
+    }
+}; export default Home;
--- asistencia/src/index.js
+++ asistencia/src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Home from './components/home';
+import Login from './components/login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        {/* <App /> */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
